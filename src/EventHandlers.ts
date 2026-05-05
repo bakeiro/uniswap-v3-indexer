382d87a47a8940e7f3bf73063d4481ea901ffef8
Bakeiro <davidbaqueiro@outlook.com>
2026-05-05 13:49:37 +0200

    extreme optimization

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,6 +1,2 @@
-import './handlers/poolCreated';
-import './handlers/initialize';
-import './handlers/collect';
-import './handlers/burn';
 import './handlers/mint';
-import './handlers/swap';
+import './handlers/burn';
